c780f41369ce59f50a650751054cc3f01b5a7fda
Vastro-lorde <omatsolaseund@gmail.com>
2023-04-12 11:09:33 +0100

    changed to port settings

--- app/app.go
+++ app/app.go
@@ -1,14 +1,22 @@
 package app
 
-import(
-	"golang.org/seun/Todo/app/controllers"
+import (
+	"log"
+	"os"
 	"github.com/gin-gonic/gin"
+	"golang.org/seun/Todo/app/controllers"
 )
 
-func StartServer(){
+func StartServer() {
 	router := gin.Default()
 	router.GET("/todos", controllers.GetTodos)
 	router.GET("/", controllers.GetHome)
 
-	router.Run("localhost:5900")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
 }
